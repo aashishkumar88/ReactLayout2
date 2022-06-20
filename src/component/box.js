718f8659064b617b33be9018df08fb8fb47eea3f
rahul.kera <rahul.khera@innroad.com>
2022-06-20 14:06:52 +0530

    layout2

---
+++ src/component/box.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const box = () => {
+  return (
+    <>
+      <div class="card__item">
+        <img src="https://place-hold.it/700x400" alt="" />
+        <div class="text">
+          <span>Feature Label</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <button class="buy">Buy Now!</button>
+          <button class="info">More Info</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default box;
